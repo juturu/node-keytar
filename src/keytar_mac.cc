b22091a6b8efadb2a1c191e4aa713e09d3048f25
Way Vadhanasin <40037392+wayvad@users.noreply.github.com>
2020-08-06 07:32:46 -0700

    Keytar4 (#5)
    * Add emebbed typings
    
    * Ignore .tags files and CI configuration in npm
    
    * 4.0.3
    
    * Support unicode account names on windows
    
    * :memo:
    
    [ci-skip]
    
    * Fix typos
    
    * added test to validate the unicode support
    
    * Only build pushes on Travis for master branch
    
    [ci skip]
    
    * force the JS string to use unicode characters
    
    * 4.0.4
    
    * feat: prebuild binaries for electron and node
    
    * add electron 1.8.0
    
    * allow cross-compile for ARM, which requires static linking
    
    * :shirt:
    
    * 4.0.5
    
    * use npm again
    
    * support node 9
    
    * Adds a findCredentials method which yields an array of account/password objects of all matching saved credentials #56
    
    * Fix package-lock.json
    
    * 4.1.0
    
    * update deps
    
    * commit ugly package lock
    
    * Restore .npmignore
    
    * Only upload prebuilds for release tag builds
    
    * Rename auth token variable
    
    * Add Electron 2.0 prebuild
    
    * Update TypeScript typings to latest DefinitelyTyped version
    
    * Add findCredentials signature to keytar.d.ts
    
    * Fix FindCredentials size on windows
    
    `FindCredentials` was missing passing the `CredentialBlobSize` which resulted in an extra character at the end of the password for some values. Fixes: https://github.com/atom/node-keytar/issues/96
    
    * Break long line in src/keytar_win.cc
    
    * Test Unicode support in findCredentials
    
    * FindCredentials should return UTF-8 usernames on Windows
    
    * 4.2.0
    
    * Enable annotated tag builds in appveyor.yml
    
    * Fix missing TypeScript typings in published package
    
    * 4.2.1
    
    * fix typings
    
    * bracket type
    
    * Add N-API support.
    
    * Match the node-addon-api version.
    
    * Update package.json
    
    * Update package.json
    
    * Update README.md
    
    Co-authored-by: Milan Burda <miburda@microsoft.com>
    Co-authored-by: Ash Wilson <smashwilson@github.com>
    Co-authored-by: Julien Roncaglia <julien@roncaglia.fr>
    Co-authored-by: Nguyen Long Nhat <torn4dom4n@gmail.com>
    Co-authored-by: Wliu <50Wliu@users.noreply.github.com>
    Co-authored-by: Brendan Forster <brendan@github.com>
    Co-authored-by: Brendan Forster <github@brendanforster.com>
    Co-authored-by: Michelle Tilley <binarymuse@github.com>
    Co-authored-by: Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
    Co-authored-by: develar <develar@gmail.com>
    Co-authored-by: Steve Desmond <steve@vtsv.ca>
    Co-authored-by: Damien Guard <damien@envytech.co.uk>
    Co-authored-by: Damien Guard <damieng@gmail.com>
    Co-authored-by: Michael Siebert <michael.siebert@samedi.de>
    Co-authored-by: David Wilson <daviwil@github.com>
    Co-authored-by: Dotta <34892728+cryppadotta@users.noreply.github.com>
    Co-authored-by: Michael Wei <mwei@cs.ucsd.edu>
    Co-authored-by: Lee Dohm <1038121+lee-dohm@users.noreply.github.com>

--- src/keytar_mac.cc
+++ src/keytar_mac.cc
@@ -1,8 +1,42 @@
-#include "keytar.h"
-
 #include <Security/Security.h>
+#include "keytar.h"
+#include "credentials.h"
+
 
 namespace keytar {
+
+/**
+ * Converts a CFString to a std::string
+ *
+ * This either uses CFStringGetCStringPtr or (if that fails)
+ * CFStringGetCString, trying to be as efficient as possible.
+ */
+const std::string CFStringToStdString(CFStringRef cfstring) {
+  const char* cstr = CFStringGetCStringPtr(cfstring, kCFStringEncodingUTF8);
+
+  if (cstr != NULL) {
+    return std::string(cstr);
+  }
+
+  CFIndex length = CFStringGetLength(cfstring);
+  // Worst case: 2 bytes per character + NUL
+  CFIndex cstrPtrLen = length * 2 + 1;
+  char* cstrPtr = static_cast<char*>(malloc(cstrPtrLen));
+
+  Boolean result = CFStringGetCString(cfstring,
+                                      cstrPtr,
+                                      cstrPtrLen,
+                                      kCFStringEncodingUTF8);
+
+  std::string stdstring;
+  if (result) {
+    stdstring = std::string(cstrPtr);
+  }
+
+  free(cstrPtr);
+
+  return stdstring;
+}
 
 const std::string errorStatusToString(OSStatus status) {
   std::string errorStr;
@@ -23,8 +57,7 @@ const std::string errorStatusToString(OSStatus status) {
 KEYTAR_OP_RESULT AddPassword(const std::string& service,
                              const std::string& account,
                              const std::string& password,
-                             std::string* error,
-                             bool returnNonfatalOnDuplicate) {
+                             std::string* error) {
   OSStatus status = SecKeychainAddGenericPassword(NULL,
                                                   service.length(),
                                                   service.data(),
@@ -34,9 +67,7 @@ KEYTAR_OP_RESULT AddPassword(const std::string& service,
                                                   password.data(),
                                                   NULL);
 
-  if (status == errSecDuplicateItem && returnNonfatalOnDuplicate) {
-    return FAIL_NONFATAL;
-  } else if (status != errSecSuccess) {
+  if (status != errSecSuccess) {
     *error = errorStatusToString(status);
     return FAIL_ERROR;
   }
@@ -48,16 +79,30 @@ KEYTAR_OP_RESULT SetPassword(const std::string& service,
                              const std::string& account,
                              const std::string& password,
                              std::string* error) {
-  KEYTAR_OP_RESULT result = AddPassword(service, account, password,
-                                        error, true);
-  if (result == FAIL_NONFATAL) {
-    // This password already exists, delete it and try again.
-    KEYTAR_OP_RESULT delResult = DeletePassword(service, account, error);
-    if (delResult == FAIL_ERROR)
-      return FAIL_ERROR;
-    else
-      return AddPassword(service, account, password, error, false);
-  } else if (result == FAIL_ERROR) {
+  SecKeychainItemRef item;
+  OSStatus result = SecKeychainFindGenericPassword(NULL,
+                                                   service.length(),
+                                                   service.data(),
+                                                   account.length(),
+                                                   account.data(),
+                                                   NULL,
+                                                   NULL,
+                                                   &item);
+
+  if (result == errSecItemNotFound) {
+    return AddPassword(service, account, password, error);
+  } else if (result != errSecSuccess) {
+    *error = errorStatusToString(result);
+    return FAIL_ERROR;
+  }
+
+  result = SecKeychainItemModifyAttributesAndData(item,
+                                                  NULL,
+                                                  password.length(),
+                                                  password.data());
+  CFRelease(item);
+  if (result != errSecSuccess) {
+    *error = errorStatusToString(result);
     return FAIL_ERROR;
   }
 
@@ -146,6 +191,99 @@ KEYTAR_OP_RESULT FindPassword(const std::string& service,
   *password = std::string(reinterpret_cast<const char*>(data), length);
   SecKeychainItemFreeContent(NULL, data);
   CFRelease(item);
+  return SUCCESS;
+}
+
+Credentials getCredentialsForItem(CFDictionaryRef item) {
+  CFStringRef service = (CFStringRef) CFDictionaryGetValue(item,
+                                                           kSecAttrService);
+  CFStringRef account = (CFStringRef) CFDictionaryGetValue(item,
+                                                           kSecAttrAccount);
+
+  CFMutableDictionaryRef query = CFDictionaryCreateMutable(
+    NULL,
+    0,
+    &kCFTypeDictionaryKeyCallBacks,
+    &kCFTypeDictionaryValueCallBacks);
+
+  CFDictionaryAddValue(query, kSecAttrService, service);
+  CFDictionaryAddValue(query, kSecClass, kSecClassGenericPassword);
+  CFDictionaryAddValue(query, kSecMatchLimit, kSecMatchLimitOne);
+  CFDictionaryAddValue(query, kSecReturnAttributes, kCFBooleanTrue);
+  CFDictionaryAddValue(query, kSecReturnData, kCFBooleanTrue);
+  CFDictionaryAddValue(query, kSecAttrAccount, account);
+
+  CFTypeRef result;
+  OSStatus status = SecItemCopyMatching((CFDictionaryRef) query, &result);
+
+  if (status == errSecSuccess) {
+      CFDataRef passwordData = (CFDataRef) CFDictionaryGetValue(
+        (CFDictionaryRef) result,
+        CFSTR("v_Data"));
+      CFStringRef password = CFStringCreateFromExternalRepresentation(
+        NULL,
+        passwordData,
+        kCFStringEncodingUTF8);
+
+      Credentials cred = Credentials(
+        CFStringToStdString(account),
+        CFStringToStdString(password));
+      CFRelease(password);
+
+      return cred;
+  }
+
+  return Credentials();
+}
+
+KEYTAR_OP_RESULT FindCredentials(const std::string& service,
+                                 std::vector<Credentials>* credentials,
+                                 std::string* error) {
+  CFStringRef serviceStr = CFStringCreateWithCString(
+    NULL,
+    service.c_str(),
+    kCFStringEncodingUTF8);
+
+  CFMutableDictionaryRef query = CFDictionaryCreateMutable(
+    NULL,
+    0,
+    &kCFTypeDictionaryKeyCallBacks,
+    &kCFTypeDictionaryValueCallBacks);
+  CFDictionaryAddValue(query, kSecClass, kSecClassGenericPassword);
+  CFDictionaryAddValue(query, kSecAttrService, serviceStr);
+  CFDictionaryAddValue(query, kSecMatchLimit, kSecMatchLimitAll);
+  CFDictionaryAddValue(query, kSecReturnRef, kCFBooleanTrue);
+  CFDictionaryAddValue(query, kSecReturnAttributes, kCFBooleanTrue);
+
+  CFTypeRef result;
+  OSStatus status = SecItemCopyMatching((CFDictionaryRef) query, &result);
+
+  if (status == errSecSuccess) {
+    CFArrayRef resultArray = (CFArrayRef) result;
+    int resultCount = CFArrayGetCount(resultArray);
+
+    for (int idx = 0; idx < resultCount; idx++) {
+      CFDictionaryRef item = (CFDictionaryRef) CFArrayGetValueAtIndex(
+        resultArray,
+        idx);
+
+      Credentials cred = getCredentialsForItem(item);
+      credentials->push_back(cred);
+    }
+  } else if (status == errSecItemNotFound) {
+    return FAIL_NONFATAL;
+  } else {
+    *error = errorStatusToString(status);
+    return FAIL_ERROR;
+  }
+
+
+  if (result != NULL) {
+    CFRelease(result);
+  }
+
+  CFRelease(query);
+
   return SUCCESS;
 }
 
